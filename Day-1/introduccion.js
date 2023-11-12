01343e3b9bc0cb9bc20b7aba44bc778e0d8f38e5
GabrielVelozo <gabittovelozo@gmail.com>
2023-11-12 20:20:41 -0300

    [30-Days-Of-Javascript] 1-Days

---
+++ Day-1/introduccion.js
@@ -0,0 +1,48 @@
+// 1 - Escribe un comentario de una sola línea que diga "los comentarios pueden hacer que el código sea legible"
+// Los comentarios pueden hacer que el codigo sea legible.
+
+// 2 - Escribe otro comentario que diga, "Bienvenido a 30DaysOfJavaScript" 
+// 'Bienvenido a 30DaysOfJavascript'
+
+// 3 - Escribe un comentario multilínea que diga, "Los comentarios pueden hacer el código legible, fácil de usar e informativo"
+/* "Los comentarios pueden hacer el código legible, fácil de usar e informativo" */
+
+// 4 - Crea un archivo variable.js y declarar variables y asignar tipos de datos string, booleanos, indefinidos y nulos
+let palabra='Esto es un String';
+let booleanos=true;
+let indefinidos;
+let nulos= null;
+
+// 5 - Crea el archivo datatypes.js y utilizar el operador "typeof" de JavaScript para comprobar los diferentes tipos de datos. Comprueba el tipo de datos de cada variable
+console.log(typeof(palabra));
+console.log(typeof(booleanos));
+console.log(typeof(indefinidos));
+console.log(typeof(nulos));
+
+// 6 - Declara cuatro variables sin asignar valores
+let primera;
+let segunda;
+let tercera;
+let cuarta;
+
+// 7 - Declara cuatro variables con valores asignados
+let string1='Prueba dos';
+let booleanos2= false;
+let numeros= 12;
+let numerosFlotantes= 12.5;
+
+// 8 - Declara variables para almacenar su nombre, apellido, estado civil, país y edad en múltiples líneas
+const miNombre= 'Gabriel';
+const miApellido= 'Velozo';
+const estadoCivil= 'Concubino';
+const miPais= 'Argentina';
+let miEdad= 34;
+
+// 9 - Declara variables para almacenar su nombre, apellido, estado civil, país y edad en una sola línea
+let nombre='Gabriel', apellido='Velozo', eCivil='Concubino', pais='Argentina', edad= 34;
+
+// 10 - Declara dos variables miEdad y tuEdad y asígneles valores iniciales y regístrese en la consola del navegador.
+let mEdad= 34;
+let tuEdad= 32;
+console.log(miEdad);
+console.log(tuEdad);
